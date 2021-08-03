3451acaf0cd536f03fa0cfa2f332c039f0312d76
Alex Nelson <alexander.k.nelson@vanderbilt.edu>
2021-08-02 21:47:46 -0700

    Changed theme to dark mode, changed landing page

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,21 @@
 from flask import Flask
-
+from flask_login import LoginManager
+from User.flask_auth import auth as auth_blueprint
+from main import main as main_blueprint
 
 # def create_app():
 app = Flask(__name__)
-
+login_manager = LoginManager()
 app.config['SECRET_KEY'] = 'put super secret passcode here'
 
-from User.flask_auth import auth as auth_blueprint
+
 app.register_blueprint(auth_blueprint)
 
-from main import main as main_blueprint
+
 app.register_blueprint(main_blueprint)
 
 # return app
 
 if __name__ == '__main__':
+    login_manager.init_app(app)
     app.run(debug=True)
