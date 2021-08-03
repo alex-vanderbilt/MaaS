6890790f82131ad48abe95c322cc23e3b00149c3
Alex Nelson <alexander.k.nelson@vanderbilt.edu>
2021-08-03 15:59:43 -0700

    DB fully re-written, everything operational

--- app/flask_auth.py
+++ app/flask_auth.py
@@ -2,13 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import User
-
-from User.user_credentials import UserCredentials
-from User.user_auth import authenticated_user
-import pyotp
 from __init__ import db
-
-from AMC.AMC import AMCMovie
 from AMC.AMCRequest import AMCRequest
 
 auth = Blueprint('auth', __name__)
@@ -20,6 +14,7 @@ def search_zip():
     request_value = str(request.form.get('theaterList'))
     # TODO - Check this input data from the user. Likely use some zip_code library
     requester = AMCRequest()
+    theater_list = None
     if zip_code != "None":
         current_user.last_searched_zip = zip_code
         db.session.commit()
@@ -37,26 +32,16 @@ def search_zip():
             i += 1
     return render_template('profile.html', theater_list=theater_list)
 
+
 @auth.route('/update_preferences', methods=['POST'])
 def save_preferences():
     day_of_week = str(request.form.get('day_of_week'))
     time_of_day = str(request.form.get('time_of_day'))
-    # Because we automatically select "Monday" and "Morning" the form should never return None
-    database_credentials = UserCredentials("testDB.db", "user_creds")
-    database_credentials.access_database("WOOO!!!!!!!!!!")
-    database_credentials.update_notification_day(authenticated_user.username, day_of_week)
-    database_credentials.update_time_of_day(authenticated_user.username, time_of_day)
-    database_credentials.close_database()
-    authenticated_user.set_day_of_week(day_of_week)
-    authenticated_user.set_time_of_day(time_of_day)
-    zip_code = "None"
-    theater_list = None
-    return render_template('profile.html', theater_list=theater_list, zip_code=zip_code,
-                           verified_user=authenticated_user.verified,
-                           first_name=authenticated_user.first_name,
-                           favorited_theater=authenticated_user.theater_string,
-                           current_user=authenticated_user,
-                           is_fav_theater=authenticated_user.favorite_theater_name)
+    current_user.preferred_time = time_of_day
+    current_user.preferred_day = day_of_week
+    db.session.commit()
+    return render_template('profile.html', theater_list=None)
+
 
 @auth.route('/login', methods=['POST'])
 def login_post():
@@ -75,67 +60,21 @@ def login_post():
         login_user(user, remember=remember)
         return redirect(url_for('main.profile'))
 
-    # database_credentials = UserCredentials("testDB.db", "user_creds")
-    # database_credentials.access_database("WOOO!!!!!!!!!!")
-
-    # login_user = database_credentials.authenticate_user(name, password)
-    #
-    # if login_user:
-    #     database_user_information = database_credentials.fetch_entire_table()
-    #     for user in database_user_information:
-    #         if name == str(user[0]):
-    #             authenticated_user.update_user(user[0], user[2], user[3], user[4], user[5], user[6], user[7], user[9],
-    #                                            user[11])
-    #             authenticated_user.theater_string = database_credentials.fetch_users_favorited_theater(
-    #                 authenticated_user.username)
-    #             authenticated_user.favorite_theater_name = database_credentials.fetch_user_theater_name(
-    #                 authenticated_user.username)
-    #             authenticated_user.preferred_day = database_credentials.fetch_desired_notification_day(
-    #                 authenticated_user.username)
-    #             authenticated_user.preferred_time = database_credentials.fetch_desired_notification_time(
-    #                 authenticated_user.username)
-    #             database_credentials.close_database()
-    #             # if user[10] == "false":
-    #             #     return redirect(url_for('auth.two_factor_enroll'))
-    #             # else:
-    #             #     return redirect(url_for('auth.two_factor_verification'))
-    #             return redirect(url_for('main.profile_post', verified_user=authenticated_user.verified,
-    #                                     first_name=authenticated_user.first_name,
-    #                                     favorited_theater=authenticated_user.theater_string,
-    #                                     current_user=authenticated_user,
-    #                                     is_fav_theater=authenticated_user.favorite_theater_name,
-    #                                     name=authenticated_user.username))
-    # else:
-    #     # print("here")
-    #     database_credentials.close_database()
-    #     flash('Please check your login details and try again')
-    #     return redirect(url_for('auth.login', verified_user=authenticated_user.verified,
-    #                             first_name=authenticated_user.first_name, favorited_theater=authenticated_user.theater_string,
-    #                             name=authenticated_user.username))
-
 
 @auth.route('/login')
 def login():
-    return render_template('login.html', verified_user=authenticated_user.verified,
-                           first_name=authenticated_user.first_name, favorited_theater=authenticated_user.theater_string,
-                           name=authenticated_user.username)
+    return render_template('login.html')
 
 
 @auth.route('/signup')
 def signup():
-    return render_template('signup.html', verified_user=authenticated_user.verified,
-                           first_name=authenticated_user.first_name, favorited_theater=authenticated_user.theater_string,
-                           name=authenticated_user.username)
+    return render_template('signup.html')
 
 
 @auth.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
-    # authenticated_user.log_out_user()
-    # return redirect(url_for('auth.login', verified_user=authenticated_user.verified,
-    #                         first_name=authenticated_user.first_name, favorited_theater=authenticated_user.theater_string,
-    #                         name=authenticated_user.username))
 
 
 @auth.route('/signup', methods=['POST'])
@@ -172,70 +111,3 @@ def signup_post():
     db.session.commit()
 
     return redirect(url_for('auth.login'))
-    # database_credentials = UserCredentials("testDB.db", "user_creds")
-    # database_credentials.access_database("WOOO!!!!!!!!!!")
-
-    # key_in_database = database_credentials.fetch_table_column("login_username")
-
-    # for entry in key_in_database:
-    #     if entry == name:
-    #         flash('Username is already in use')
-    #         print('Username is already in use')
-    #         return redirect(url_for('auth.signup', verified_user=authenticated_user.verified,
-    #                                 first_name=authenticated_user.first_name, favorited_theater=authenticated_user.theater_string,
-    #                                 name=authenticated_user.username))
-    #
-    # hashed_password = generate_password_hash(password)
-    # database_credentials.populate_table(name, hashed_password, email, first_name, last_name, phone_number, zipcode,
-    #                                     preferred_comm, favorite_theater, favorite_genre)
-    # database_credentials.commit_changes()
-    # database_credentials.close_database()
-    # test_print = database_credentials.fetch_entire_table()
-    # print(test_print)
-
-    # return redirect(url_for('auth.login', verified_user=authenticated_user.verified,
-    #                         first_name=authenticated_user.first_name, favorited_theater=authenticated_user.theater_string,
-    #                         name=authenticated_user.username))
-
-
-# @auth.route('/2FA/Enrollment', methods=['Get', 'POST'])
-# def two_factor_enroll():
-#     database_credentials = UserCredentials("testDB.db", "user_creds")
-#     database_credentials.access_database("WOOO!!!!!!!!!!")
-#
-#     if request.method == 'POST':
-#         # print("here")
-#         otp = int(request.form.get("otp"))
-#         if pyotp.TOTP(authenticated_user.secret_key).verify(otp):
-#             database_credentials.update_user_enrollment(authenticated_user.username)
-#             database_credentials.commit_changes()
-#             database_credentials.close_database()
-#             flash("You have successfully enrolled 2FA for your profile, please authenticate yourself once more!",
-#                   "success")
-#             return redirect(url_for('auth.two_factor_verification'))
-#         else:
-#             database_credentials.close_database()
-#             flash("The OTP provided is invalid, it has either expired or was generated using a wrong SECRET!", "danger")
-#             return redirect(url_for('auth.two_factor_enroll'))
-#
-#     return render_template('2faenroll.html', secret=authenticated_user.secret_key, favorited_theater=authenticated_user.theater_string,
-#                            name=authenticated_user.username)
-#
-#
-# @auth.route('/2FA/Verification', methods=['Get', 'POST'])
-# def two_factor_verification():
-#     if request.method == "POST":
-#         otp = int(request.form.get("otp"))
-#
-#         if pyotp.TOTP(authenticated_user.secret_key).verify(otp):
-#             authenticated_user.user_authenticated()
-#             return redirect(url_for('main.profile_post', verified_user=authenticated_user.verified,
-#                                     first_name=authenticated_user.first_name, favorited_theater=authenticated_user.theater_string,
-#                                     current_user=authenticated_user,
-#                                     is_fav_theater=authenticated_user.favorite_theater_name,
-#                                     name=authenticated_user.username))
-#         else:
-#             flash("The OTP provided is invalid, it has either expired or was generated using a wrong SECRET!", "danger")
-#             return redirect(url_for('auth.two_factor_verification'))
-#
-#     return render_template('2faverify.html')
--- app/main.py
+++ app/main.py
@@ -1,27 +1,9 @@
-from flask import Blueprint, render_template, redirect, url_for
-from flask_login import login_required, current_user
-from User.user_auth import authenticated_user
-from User.user_credentials import UserCredentials
-from AMC.AMC import AMCMovie
+from flask import Blueprint, render_template
+from flask_login import login_required
 from AMC.AMCRequest import AMCRequest
-from __init__ import db
 
 
 main = Blueprint('main', __name__)
-
-
-
-# application = Flask(__name__)
-# application.config['SECRET_KEY'] = 'put super secret passcode here'
-
-# if __name__ == '__main__':
-    # from User.flask_auth import auth as auth_blueprint
-    # application.register_blueprint(auth_blueprint)
-    #
-    # import main as main_blueprint
-    # application.register_blueprint(main_blueprint)
-
-    # return application
 
 
 @main.route('/')
@@ -29,55 +11,9 @@ def index():
     requester = AMCRequest()
     current_movie_list = requester.get_current_movies()
     return render_template('index.html', movie_list=current_movie_list)
-    # database_credentials = UserCredentials("testDB.db", "user_creds")
-    # database_credentials.access_database("WOOO!!!!!!!!!!")
-    # link = database_credentials.get_new_movie_trailer()
-    # database_credentials.close_database()
-    # # if authenticated_user.authenticated == "False":
-    # #     authenticated_user.log_out_user()
-    # # database_credentials.create_movie_trailer_table()
-    # # database_credentials.populate_movie_trailer_table()
-    # requester = AMCRequest()
-    # current_movie_list = requester.get_current_movies()
-    # return render_template('index.html', verified_user=authenticated_user.verified, movie_list = current_movie_list,
-    #                        first_name=authenticated_user.first_name, link=str(link),
-    #                        favorited_theater=authenticated_user.theater_string,
-    #                        name=authenticated_user.username)
-
-
-# @main.route('/profile/Welcome')
-# def profile_post():
-#     requester = AMCRequest()
-#     current_movie_list = requester.get_current_movies()
-#     return render_template('profile.html', theater_list=None, verified_user=authenticated_user.verified,
-#                            first_name=authenticated_user.first_name,
-#                            favorited_theater=authenticated_user.theater_string,
-#                            is_fav_theater=authenticated_user.favorite_theater_name,
-#                            current_user=authenticated_user)
 
 
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', username=current_user.username)
-    # if authenticated_user.username is None:
-    #     return redirect(url_for('auth.login', verified_user=authenticated_user.verified,
-    #                             first_name=authenticated_user.first_name, favorited_theater=authenticated_user.theater_string,
-    #                             name=authenticated_user.username))
-    # else:
-    #     return redirect(url_for('main.profile_post', verified_user=authenticated_user.verified,
-    #                             first_name=authenticated_user.first_name, favorited_theater=authenticated_user.theater_string,
-    #                             is_fav_theater=authenticated_user.favorite_theater_name,
-    #                             current_user=authenticated_user,
-    #                             name=authenticated_user.username,))
-
-
-# @main.route('/developer_console')
-# def developer():
-#     return render_template('main.profile_post', theater_list=None,
-#                            verified_user=authenticated_user.verified, first_name=authenticated_user.first_name,
-#                            favorited_theater=authenticated_user.theater_string,
-#                            is_fav_theater=authenticated_user.favorite_theater_name,
-#                            current_user=authenticated_user,
-#                            name=authenticated_user.username,
-#                            )
+    return render_template('profile.html', theater_list=None)
